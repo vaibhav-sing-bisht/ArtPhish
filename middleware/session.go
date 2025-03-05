ad2c435348f63ee68167d4a5691a37ba49a93afe
David Busby <david.busby@quiq.com>
2025-03-05 14:33:52 +0000

    Post depencies update facing issues with succesfull login, seeing that the dateime for the logged in user is updated in gophish.users table in MySQL however the sesison is not able to persist, oddly however we see this is fine when sqlite is used, this commit adds login to middleware/session.go to add Secure flag when setting cookies via HTTPS as well as SameSite=None

--- middleware/session.go
+++ middleware/session.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"encoding/gob"
+	"net/http"
 
 	"github.com/gophish/gophish/models"
 	"github.com/gorilla/securecookie"
@@ -13,8 +14,13 @@ func init() {
 	gob.Register(&models.User{})
 	gob.Register(&models.Flash{})
 	Store.Options.HttpOnly = true
+	//Secure cookies if request is over HTTPS
+	Store.Options.Secure = true                    // Ensure cookies are marked as Secure
+	Store.Options.SameSite = http.SameSiteNoneMode // Explicitly set SameSite=None
+	// This sets the maxAge to 5 days for all cookies
 	// This sets the maxAge to 5 days for all cookies
 	Store.MaxAge(86400 * 5)
+
 }
 
 // Store contains the session information for the request
